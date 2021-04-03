0d2369dc5dadf0d937d4ff3cbcbd814ba4fd7572
Antonio Ribeiro <antonio.ribeiro@mercedes-benz.io>
2021-04-03 19:43:38 +0100

    Working web server.
    Requirements:
    - User data;
    - Key-pair name;
    - Inbound rules for SSH and HTTP;
    - Generic outbound rule.

--- main.tf
+++ main.tf
@@ -5,9 +5,46 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0d6aecf0f0425f42a"
-  instance_type = "t2.micro"
+  ami                     = "ami-00dd995cb6f0a5219"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = [aws_security_group.instance.id]
+  key_name  = "Terraform"
+  user_data = <<-EOF
+              #!/bin/bash
+              # Use this for your user data (script without newlines)
+              # install httpd (Linux 2 version)
+              yum update -y
+              yum install -y httpd.x86_64
+              systemctl start httpd.service
+              systemctl enable httpd.service
+              echo "Hello Terraform World from $(hostname -f)" > /var/www/html/index.html
+              EOF
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
